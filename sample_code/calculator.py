5fdc5a384bbe242c7b910e75bf827ab26cddeff3
Gabriyel <g4briyyyel@gmail.com>
2026-09-19 08:55:07 +0800

    Add calculator code

--- sample_code/calculator.py
+++ sample_code/calculator.py
@@ -2,7 +2,9 @@ num1 = 10
 num2 = 5
 
 sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
 
-print("First number:", num1)
-print("Second number:", num2)
 print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
